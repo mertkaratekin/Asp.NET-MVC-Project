dab1ffac866c7fad61b3ba1e7294385a0f754a23
Mert Karatekin <mert1616karatekin@gmail.com>
2023-07-20 11:27:47 +0300

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BnazHow.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            //veritabanından bütün blog bilgilerini al ve View üzerine gönder. 
+            //Kendisine gelen blog bilgilerini dinamik içerik üreten view yapısı bunu statik html 'e çevirir. 
+            
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+     
+    }
+}
